76c1fe28abc8e052b1e22eaad53dcbe34504f3c5
Leo Moser <leomoser99@gmail.com>
2026-08-17 14:44:07 +0200

    feat: add yosys primitives
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

---
+++ ip/extra-tiles/primitives/CPU_IRQ/yosys/primitives/CPU_IRQ.v
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: © 2026 Leo Moser
+// SPDX-License-Identifier: Apache-2.0
+
+(* blackbox, keep *)
+module CPU_IRQ (
+    input  IRQ0,
+    input  IRQ1,
+    input  IRQ2,
+    input  IRQ3
+);
+
+endmodule
